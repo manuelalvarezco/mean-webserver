0caa94b488a809bd9362e43c31833ba0d14ee914
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2020-08-04 23:12:01 -0500

    inicio CRUD

---
+++ server/routes/users.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const bcrypt = require('bcrypt');
+const _ = require('underscore')
+
+const User = require('../models/user')
+
+const app = express()
+
+app.get('/users', function(req, res) {
+
+    let from = req.query.from || 0;
+    from = Number(from);
+    let limit = req.query.limit || 0;
+    limit = Number(limit);
+
+    User.find({ status: true, role: 'USER_ROLE' })
+        .skip(from)
+        .limit(limit)
+        .exec((err, users) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            User.countDocuments({ status: true }, (err, count) => {
+
+                res.json({
+                    ok: true,
+                    users,
+                    count
+                })
+            })
+
+        })
+
+
+});
+
+
+app.post('/users', function(req, res) {
+
+    let body = req.body;
+
+    let user = new User({
+        name: body.name,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role,
+    })
+
+    user.save((err, userDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDB
+        })
+
+
+    })
+
+});
+
+app.put('/users/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['name', 'email', 'password', 'img', 'status']);
+
+
+    User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, userDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDB
+        })
+
+    })
+});
+
+
+app.delete('/users', function(req, res) {
+    res.json('DELETE usuarios');
+});
+
+
+module.exports = app
---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('./config/config.js')
+
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+const cors = require('cors');
+
+app.use(express.static(__dirname + '/public'))
+app.use(cors());
+
+
+app.use(require('./routes/users'))
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err
+
+    console.log('Base de datos online');
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando el purto ${process.env.PORT}`);
+
+})
